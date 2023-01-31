6af0c6de10abb890ccfa0fd766b10ad58db87e8b
Guilherme Placido <guilherme333placido@gmail.com>
2023-01-31 10:23:10 -0300

    menu mobile adicionado

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,21 @@ const botaoAbrirModal = document.querySelector(".botao-video");
 const botaoFecharModal = document.querySelector(".botao-fechar");
 const modalContainer = document.querySelector(".modal-container");
 const video = document.getElementById("popup-video");
+const btnMobile = document.getElementById("btn-mobile");
 
 function alternarModal() {
     modal.classList.toggle("aberto");
     video.setAttribute("src", "https://www.youtube.com/embed/K4TOrB7at0Y");
 }
 
+function toggleMenu() {
+    const nav = document.getElementById("nav");
+    nav.classList.toggle("active");
+}
+
 botaoAbrirModal.addEventListener("click", alternarModal);
 botaoFecharModal.addEventListener("click", alternarModal);
+btnMobile.addEventListener('click', toggleMenu);
 
 modalContainer.addEventListener("click", () => {
     modal.classList.remove("aberto");
